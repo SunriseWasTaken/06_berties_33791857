c614cec6d1ac6fc3fa5ed3b4f596fd492411a54c
SunriseWasTaken <rafipalma13@gmail.com>
2025-11-15 09:29:52 +0000

    Add route to show add book form and handle book submission to database

--- routes/books.js
+++ routes/books.js
@@ -23,5 +23,33 @@ router.get('/list', function(req, res, next) {
      });
 });
 
+// show the add book form
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs');
+});
+
+// handle form submission and insert into database
+router.post('/bookadded', function (req, res, next) {
+
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.send(
+                'This book is added to the database. name: ' +
+                req.body.name +
+                ' price ' +
+                req.body.price
+            );
+        }
+    });
+});
+
 // Export the router object so index.js can access it
 module.exports = router
