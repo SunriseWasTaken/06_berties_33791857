944de9210125fefa5ddde651abe18acc80dc38fb
SunriseWasTaken <rafipalma13@gmail.com>
2025-11-23 21:57:24 +0000

    Lab 7 - Task 1 & 2

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,37 @@
 const express = require("express")
 const router = express.Router()
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-}); 
+    const plainPassword = req.body.password
+    bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+
+        if (err) { return next(err) }
+
+        let sqlquery = "INSERT INTO users (username, first, last, email, hashedPassword) VALUES (?,?,?,?,?)"
+
+        let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword]
+
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) { return next(err) }
+            else {
+                result = 'Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered!  We will send an email to you at ' + req.body.email
+                result += 'Your password is: ' + req.body.password + ' and your hashed password is: ' + hashedPassword
+                res.send(result)
+            }
+
+        })
+
+    })
+});
+
+
 
 // Export the router object so index.js can access it
 module.exports = router
