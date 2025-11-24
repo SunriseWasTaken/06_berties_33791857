d8586a4fb2183820cf96d27a1233afe56c67c0f8
SunriseWasTaken <rafipalma13@gmail.com>
2025-11-24 01:03:05 +0000

    Enhance database setup by adding user insertion SQL and fixing dotenv import in index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
+
 // Import express and ejs
 var express = require ('express')
 var ejs = require('ejs')
 const path = require('path')
 var mysql = require('mysql2');
-require('dotenv').config
 
 // Create the express application object
 const app = express()
@@ -31,6 +32,7 @@ const db = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
+
 global.db = db;
 
 // Load the route handlers
