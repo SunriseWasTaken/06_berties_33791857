1f65894e2283daa9f755a1b54340076c3a4ae974
SunriseWasTaken <rafipalma13@gmail.com>
2025-11-27 12:05:07 +0000

    Lab 8a - task 3.6

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,14 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId ) {
+    res.redirect('./login') // redirect to the login page
+  } else { 
+      next (); // move to the next middleware function
+  } 
+}
+
 router.get("/register", function (req, res, next) {
   res.render("register.ejs");
 });
@@ -49,7 +57,7 @@ router.post("/registered", function (req, res, next) {
   });
 });
 
-router.get("/list", function (req, res, next) {
+router.get("/list", redirectLogin, function (req, res, next) {
   let sqlquery = "SELECT username, first, last, email FROM users";
   db.query(sqlquery, (err, result) => {
     if (err) {
@@ -82,6 +90,9 @@ router.post("/loggedin", function (req, res, next) {
 
     bcrypt.compare(password, hashedPassword, function (err, same) {
       if (same === true) {
+        // Save user session here, when login is successful
+        req.session.userId = req.body.username;
+
         // log successful login
         let logQuery =
           "INSERT INTO audit (username, action, time) VALUES (?, 'success', NOW())";
@@ -108,7 +119,7 @@ router.post("/loggedin", function (req, res, next) {
   });
 });
 
-router.get("/audit", function (req, res, next) {
+router.get("/audit", redirectLogin, function (req, res, next) {
   let sqlquery = "SELECT * FROM audit ORDER BY time DESC";
 
   db.query(sqlquery, (err, result) => {
